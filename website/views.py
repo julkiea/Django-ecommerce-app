6d09e6fba89630276b84382ffe550958ae154c59
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-07 19:52:54 +0200

    Create new view: about_us

--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
