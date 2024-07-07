8476c3dbb2f1c4b025afbc4853a2210dbd04253a
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-07 18:51:11 +0200

    Change plural form of category

--- website/models.py
+++ website/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    
+    class Meta():
+        verbose_name_plural = "categories"
 
 # Create a model of products
 class Product(models.Model):
