fac18f0815007d0c26b80b8e8eb7087e9f466062
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-07 19:49:47 +0200

    Update Product model: add is_on_sale boolean field and sale_price decimal field

--- website/models.py
+++ website/models.py
@@ -18,6 +18,11 @@ class Product(models.Model):
     description = models.CharField(max_length=2500, default='', blank= True, null = True)
     image = models.ImageField(upload_to='uploads/products/')
 
+    # Add sale
+    is_on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+
+
     def __str__(self):
         return f'{self.name}'
 
