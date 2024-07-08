ee2a222707dd5b66114fddfec17b4306ccc0d72e
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-08 21:16:08 +0200

    Add login url

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('about_us', views.about_us, name = 'about_us'),
+    path('about_us/', views.about_us, name = 'about_us'),
+    path('login/', views.login, name = 'login'),
 ]
