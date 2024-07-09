4b352ad76c3a60c740cd8731b1d8a279867517c0
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-09 12:00:29 +0200

    Add new urls: login_user and logout_user

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about_us/', views.about_us, name = 'about_us'),
-    path('login/', views.login, name = 'login'),
+    path('login/', views.login_user, name = 'login_user'),
+    path('logout/', views.logout_user, name = 'logout_user'),
 ]
