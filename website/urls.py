b86be73d4f67c37d0fe0595241fc645980acce12
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-10 11:51:14 +0200

    Add new url: register_user

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about_us/', views.about_us, name = 'about_us'),
     path('login/', views.login_user, name = 'login_user'),
     path('logout/', views.logout_user, name = 'logout_user'),
+    path('register/', views.register_user, name = 'register_user'),
 ]
