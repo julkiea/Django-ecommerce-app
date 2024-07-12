f36497cfff3e32ab8694175287d01a77a090974a
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-12 17:08:27 +0200

    Create a new url: prodct

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_user, name = 'login_user'),
     path('logout/', views.logout_user, name = 'logout_user'),
     path('register/', views.register_user, name = 'register_user'),
+    path('product/<int:pk>', views.product, name = 'product'),
+
 ]
