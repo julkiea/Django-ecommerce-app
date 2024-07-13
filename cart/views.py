3b6bb0cfa0f5b95b74fbe9e62234372806fb606e
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-13 13:13:48 +0200

    Create new view: cart_summary

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def cart_summary(request):
+    return render(request, "cart_summary.html", {})
+
+def cart_add(request):
+    pass
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
